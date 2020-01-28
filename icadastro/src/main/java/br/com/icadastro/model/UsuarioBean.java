7c0f11206ba7fd65a63426a060bcb8adcd9706d7
jsantos <joaomarcosfelixds@gmail.com>
2020-01-28 15:46:46 -0300

    Adding Project

---
+++ icadastro/src/main/java/br/com/icadastro/model/UsuarioBean.java
@@ -0,0 +1,67 @@
+package br.com.icadastro.model;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+@ManagedBean(name = "usuario")
+@RequestScoped
+public class UsuarioBean {
+
+	private String nome;
+	private String login;
+	private String senha;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getLogin() {
+		return login;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((login == null) ? 0 : login.hashCode());
+		return result;
+	}
+
+	// O que identifica esse Bean unicamente é o atributo login
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		UsuarioBean other = (UsuarioBean) obj;
+		if (login == null) {
+			if (other.login != null)
+				return false;
+		} else if (!login.equals(other.login))
+			return false;
+		return true;
+	}
+	
+	public String cadastrar(){
+		return "formUsuarioSucess";
+	}
+}
